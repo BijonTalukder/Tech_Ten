f1ab0e311eb4ebbc53bc1fc1985676b63d1503ca
BijonTalukder <bijontalukder1247@gmail.com>
2023-08-03 11:58:57 +0600

    redux RTK implement

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,16 @@
 import {configureStore} from '@reduxjs/toolkit'
 import cardReducer from './features/cart/cartSlice'
+import productReducer from './features/Products/productSlice'
+import { api } from './API/apiSlice'
 const store = configureStore({
     reducer:{
-        cart:cardReducer
+        cart:cardReducer,
+        product:productReducer,
+        [api.reducerPath]:api.reducer
 
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 })
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.getState
